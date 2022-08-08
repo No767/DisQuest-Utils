a4b9de8cc0f24a0fe6c8a2e03862716feeccd021
No767 <73260931+No767@users.noreply.github.com>
2022-08-08 00:42:33 -0700

    Redo Users.py to use SQLAlchemy ORM + MyPy Stuff

--- src/disquest_utils/users.py
+++ src/disquest_utils/users.py
@@ -1,110 +1,162 @@
 import asyncio
-import os
 
 import uvloop
-from dotenv import load_dotenv
-from sqlalchemy import (BigInteger, Column, Integer, MetaData, Table, select)
-from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy import (BigInteger, Boolean, Column, Integer, String, Text,
+                        select, update, func)
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
 
-load_dotenv()
-
-PASSWORD = os.getenv("Postgres_Password")
-IP = os.getenv("Postgres_IP")
-USER = os.getenv("Postgres_User")
-DATABASE = os.getenv("Postgres_Database")
-PORT = os.getenv("Postgres_Port")
-
-
+Base = declarative_base()
+class DisQuest(Base):
+    __tablename__ = "disquest_users"
+    
+    user_id = Column(BigInteger)
+    guild_id = Column(BigInteger)
+    xp = Column(Integer)
+    
+    def __iter__(self):
+        yield "user_id", self.user_id
+        yield "guild_id", self.guild_id
+        yield "xp", self.xp
+        
+    def __repr__(self):
+        return f"DisQuest(user_id={self.user_id!r}, guild_id={self.guild_id!r}, xp={self.xp!r})"
+    
 class DisQuestUsers:
     def __init__(self):
         self.self = self
+        
+    async def initTables(self, uri: str) -> None:
+        """Initializes the tables needed for DisQuest
 
-    async def initTables(self):
-        meta = MetaData()
-        engineInit = create_async_engine(
-            f"postgresql+asyncpg://{USER}:{PASSWORD}@{IP}:{PORT}/{DATABASE}"
-        )
-        Table(
-            "users",
-            meta,
-            Column("id", BigInteger),
-            Column("gid", BigInteger),
-            Column("xp", Integer),
-        )
-        async with engineInit.begin() as connInit:
-            await connInit.run_sync(meta.create_all)
+        Args:
+            uri (str): Postgres Connection URI
+        """
+        engine = create_async_engine(uri, echo=True)
+        async with engine.begin() as conn:
+            await conn.run_sync(Base.metadata.create_all)
 
+    async def getUserXP(self, user_id: int, guild_id: int, uri: str):
+        """Gets the User's XP
+
+        Args:
+            user_id (int): Discord User ID
+            guild_id (int): Discord Guild ID
+            uri (str): Postgres Connection URI
+        """
+        engine = create_async_engine(uri)
+        async_session = sessionmaker(
+            engine, expire_on_commit=False, class_=AsyncSession
+        )
+        async with async_session() as session:
+            async with session.begin():
+                selectItem = select(DisQuest).filter(DisQuest.user_id == user_id).filter(DisQuest.guild_id == guild_id)
+                getItemSelected = await session.scalars(selectItem)
+                getItemSelectedOne = getItemSelected.first()
+                return getItemSelectedOne
+
+    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+                
+    async def insUser(self, user_id: int, guild_id: int, uri: str) -> None:
+        """Inserts a user into the DB for the first time
+
+        Args:
+            user_id (int): Discord User ID
+            guild_id (int): Discord Guild ID
+            uri (str): Postgres Connection URI
+        """
+        engine = create_async_engine(uri)
+        async_session = sessionmaker(
+            engine, expire_on_commit=False, class_=AsyncSession
+        )
+        async with async_session() as session:
+            async with session.begin():
+                selItem = select(DisQuest.xp).filter(DisQuest.user_id == user_id).filter(DisQuest.guild_id == guild_id)
+                results = await session.execute(selItem)
+                resFetchedOne = results.one()
+                fullResults = [row for row in resFetchedOne]
+                if len(fullResults) == 0:
+                    insertData = DisQuest(user_id=user_id, guild_id=guild_id, xp=0)
+                    session.add_all([insertData])
+                    await session.commit()
+                    
     asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
     
-    async def getxp(self, uid: int, gid: int):
-        meta = MetaData()
-        engine = create_async_engine(
-            f"postgresql+asyncpg://{USER}:{PASSWORD}@{IP}:{PORT}/{DATABASE}"
+    async def setUserXP(self, xp: int, user_id: int, guild_id: int, uri: str) -> None:
+        """Sets the amount of XP that a user has
+
+        Args:
+            xp (int): The amount of XP to set to
+            user_id (int): Discord User ID
+            guild_id (int): Discord Guild ID
+            uri (str): Postgres Connection URI
+        """
+        engine = create_async_engine(uri)
+        async_session = sessionmaker(
+            engine, expire_on_commit=False, class_=AsyncSession
         )
-        users = Table(
-            "users",
-            meta,
-            Column("id", BigInteger),
-            Column("gid", BigInteger),
-            Column("xp", Integer),
-        )
-        async with engine.connect() as conn:
-            s = select(users.c.xp).where(
-                users.c.id == uid, users.c.gid == gid)
-            results = await conn.execute(s)
-            results_fetched = results.fetchone()
-            for row in results_fetched:
-                return row
+        async with async_session() as session:
+            async with session.begin():
+                updateUserXP = update(DisQuest, values={DisQuest.xp: xp}).filter(DisQuest.user_id == user_id).filter(DisQuest.guild_id == guild_id)
+                await session.execute(updateUserXP)
+                await session.commit()
                 
     asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
-                
-    async def onInit(self, user_id: int, guild_id: int):
-        meta = MetaData()
-        engine = create_async_engine(
-            f"postgresql+asyncpg://{USER}:{PASSWORD}@{IP}:{PORT}/{DATABASE}"
-        )
-        users = Table(
-            "users",
-            meta,
-            Column("id", BigInteger),
-            Column("gid", BigInteger),
-            Column("xp", Integer),
-        )
-        async with engine.begin() as conn:
-            s = select(users.c.xp).filter(users.c.id == user_id).filter(users.c.gid == guild_id)
-            results = await conn.execute(s)
-            results_fetched = results.fetchone()
-            if results_fetched is None:
-                insert_new = users.insert().values(xp=0, id=user_id, gid=guild_id)
-                await conn.execute(insert_new)
-    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
-    async def setxp(self, xp: int, uid: int, gid: int):
-        meta = MetaData()
-        engine = create_async_engine(
-            f"postgresql+asyncpg://{USER}:{PASSWORD}@{IP}:{PORT}/{DATABASE}"
-        )
-        users = Table(
-            "users",
-            meta,
-            Column("id", BigInteger),
-            Column("gid", BigInteger),
-            Column("xp", Integer),
-        )
-        async with engine.begin() as conn:
-            update_values = (
-                users.update()
-                .values(xp=xp)
-                .filter(users.c.id == uid)
-                .filter(users.c.gid == gid)
-            )
-            await conn.execute(update_values)
-            
-    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+    async def addxp(self, offset: int, user_id: int, guild_id: int, uri):
+        """Adds the XP to the user
 
-    async def addxp(self, offset, uid: int, gid: int):
-        pxp = await self.getxp(uid, gid)
+        Args:
+            offset (int): The amount to offset by
+            uid (int): Discord User ID
+            gid (int): Discord Guild ID
+            uri (str): Postgres Connection URI
+        """
+        pxp = await self.getUserXP(user_id, guild_id, uri)
         pxp += offset
-        await self.setxp(pxp, uid=uid, gid=gid)
+        await self.setUserXP(pxp, user_id=user_id, guild_id=guild_id, uri=uri)
         
+    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+    
+    async def userRank(self, guild_id: int, uri: str) -> list:
+        """Gets the user's rank
+
+        Args:
+            guild_id (int): Discord Guild ID
+            uri (str): Postgres Connection URI
+
+        Returns:
+            list: List of results
+        """
+        engine = create_async_engine(uri)
+        async_session = sessionmaker(
+            engine, expire_on_commit=False, class_=AsyncSession
+        )
+        async with async_session() as session:
+            async with session.begin():
+                selectItem = select(DisQuest.user_id, DisQuest.xp).filter(DisQuest.guild_id == guild_id).order_by(DisQuest.xp.desc())
+                res = await session.execute(selectItem)
+                return [row for row in res.scalars()]
+    
+    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+
+    async def globalRank(self, uri: str) -> list:
+        """Gets the global rank
+
+        Args:
+            uri (str): Postgres Connection URI
+
+        Returns:
+            list: List of results
+        """
+        engine = create_async_engine(uri)
+        async_session = sessionmaker(
+            engine, expire_on_commit=False, class_=AsyncSession
+        )
+        async with async_session() as session:
+            async with session.begin():
+                selectGlobalRank = select(DisQuest.id, func.sum(DisQuest.xp).label("txp")).group_by(DisQuest.id).group_by(DisQuest.xp).order_by(DisQuest.xp.desc()).limit(10)
+                res = await session.execute(selectGlobalRank)
+                return [row for row in res.scalars()]
+            
     asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
